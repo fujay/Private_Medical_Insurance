c706fb418686c470a8214c2cc4559effdf681349
fujay <fujitsustar@hotmail.de>
2016-04-21 03:56:27 +0200

    Initial commit

---
+++ app/src/androidTest/java/eu/fra_uas/ochs/klamm/cesljar/gesundheitssystem/ApplicationTest.java
@@ -0,0 +1,13 @@
+package eu.fra_uas.ochs.klamm.cesljar.gesundheitssystem;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
